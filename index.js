c007acd1776e7eea1d7d7b2d8bd3210c9c3e01bc
ChristinaGraham <cgraham74@gmail.com>
2023-02-06 12:03:37 -0600

    added chrome tab function and removed doctype

--- index.js
+++ index.js
@@ -1,56 +1,46 @@
-
 let myLinks = [];
-
-const inputEl = document.getElementById('input-el');
+const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"));
+const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
 const ulEl = document.getElementById("ul-el");
 const deleteBtn = document.getElementById("delete-btn");
-const linksFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
-console.log("LS1"+linksFromLocalStorage)
+const saveTabBtn = document.getElementById("save-tab-btn");
 
-if(linksFromLocalStorage){
-    myLinks = linksFromLocalStorage;
-    render(myLinks);
+if (linksFromLocalStorage) {
+  myLinks = linksFromLocalStorage;
+  render(myLinks);
 }
 
-/**
- * Creates the list of elements and adds them to the DOM
- */
-function render(links){
-    let listItems = "";
-    for( let i = 0; i < links.length; i++){ 
-        listItems += `
+saveTabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLinks.push(tabs[0].url);
+    localStorage.setItem("myLinks", JSON.stringify(myLinks));
+    render(myLinks);
+  });
+});
+
+function render(links) {
+  let listItems = "";
+  for (let i = 0; i < links.length; i++) {
+    listItems += `
         <li>
             <a target='_blank' href='${links[i]}'>
              ${links[i]}
             </a>
-        </li>` 
-        // console.log(listItems)
-    }
-    ulEl.innerHTML = listItems;
+        </li>`;
+  }
+  ulEl.innerHTML = listItems;
 }
- 
 
-/**
- * Event Listener clears the DOM, Links, and removes elements.
- */
-deleteBtn.addEventListener("dblclick", function(){
-    localStorage.clear();
-    myLinks = [];
-    render(myLinks)
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLinks = [];
+  render(myLinks);
 });
 
-
-/**
- * Event listener for the Save button
- * It adds links to an array.
- * It then clears the input field
- * Calls the renderLinks() function
- */
-inputBtn.addEventListener("click", function(){
-    myLinks.push(inputEl.value); 
-    inputEl.value = "";
-    localStorage.setItem("myLinks", JSON.stringify(myLinks))
-    render(myLinks)
+inputBtn.addEventListener("click", function () {
+  myLinks.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLinks", JSON.stringify(myLinks));
+  render(myLinks);
 });
-console.log(myLinks)
